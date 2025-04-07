b1278079e9f26a080ab58e093d1d05868d30789e
Madhu Maragani <165878683+Maraganimadhu@users.noreply.github.com>
2025-04-07 11:07:18 +0530

    Add files via upload

---
+++ nested_list.py
@@ -0,0 +1,33 @@
+"nested list"
+# nested=[[1,2,3],[4,5,6],[7,8,9]]
+"get all elements in list and store in single list"
+
+# def nested_list(nested):
+#     singl_list=[]
+#     for i in nested:
+#         for j in i:
+#             singl_list.append(j)
+#     print(singl_list)   
+# nested_list(nested) 
+
+
+# "find the max and min values in sub list"
+# def find_min_max(neated):
+#     maxx=[]
+#     minn=[]
+#     for i in nested:
+#         max1=i[0]
+#         min1=i[0]
+#         for j in i:
+#             if max1<j:
+#                 max1=j
+#             if min1>j:
+#                 min1=j
+#         maxx.append(max1)
+#         minn.append(min1)
+#     print(f"max values in sub list is {maxx} \nmin values in sub list is {minn}")
+# find_min_max(nested)
+# """output:
+#             max values in sub list is [3, 6, 9] 
+#             min values in sub list is [1, 4, 7]
+# """
