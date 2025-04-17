61dffc2941994930c446650e6227b1cd13639003
maraganimadhu <maraganimadhu2@gmail.com>
2025-04-17 17:02:54 +0530

    updated table

--- tables.py
+++ tables.py
@@ -1,9 +1,9 @@
 "print a table 1 to 20"
 def tables(num):
-    for i in range(1,num+1):
-        for j in range(11):
-            print(f"{i} * {j}\t=\t{i*j}")
+    for table in range(1,num+1):
+        for mul in range(11):
+            print(f"{table} * {mul}\t=\t{table*mul}")
 
-        print("----------------------------------")
+        print("end of the table :",table)
 num=int(input("enter a number upto tables u want"))
 tables(num)
